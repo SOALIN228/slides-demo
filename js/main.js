c34b17e7347e406f4622a4db8a7ddb6f2613e83a
lml <soalin228@163.com>
2019-05-05 20:31:50 +0800

    无缝轮播重写

---
+++ js/main.js
@@ -0,0 +1,51 @@
+let $buttons = $('#buttonWrapper>button')
+let $slides = $('#slides')
+let $images = $slides.children('img')
+let current = 0
+
+makeFakeSlides()
+$slides.css({ transform: 'translateX(-400px)' })
+bindEvents()
+
+function bindEvents () {
+  $buttons.eq(0).on('click', function () {
+    if (current === 2) {
+      // 从最后一张到第一张
+      $slides.css({ transform: 'translateX(-1600px)' })
+      .one('transitionend', function () {
+        $slides.hide().offset()
+        $slides.css({ transform: 'translateX(-400px)' }).show()
+      })
+    } else {
+      $slides.css({ transform: 'translateX(-400px)' })
+    }
+    current = 0
+  })
+
+  $buttons.eq(1).on('click', function () {
+    $slides.css({ transform: 'translateX(-800px)' })
+    current = 1
+  })
+
+  $buttons.eq(2).on('click', function () {
+    if (current === 0) {
+      // 从第一张到最后一张
+      $slides.css({ transform: 'translateX(0px)' })
+      .one('transitionend', function () {
+        $slides.hide().offset()
+        $slides.css({ transform: 'translateX(-1200px)' }).show()
+      })
+    } else {
+      $slides.css({ transform: 'translateX(-1200px)' })
+    }
+    current = 2
+  })
+}
+
+function makeFakeSlides () {
+  let $firstCopy = $images.eq(0).clone(true)
+  let $lastCopy = $images.eq($images.length - 1).clone(true)
+
+  $slides.append($firstCopy)
+  $slides.prepend($lastCopy)
+}
