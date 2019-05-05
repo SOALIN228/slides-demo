28b33a4fde132eeafdbfb9d09549b356c076fb64
lml <soalin228@163.com>
2019-05-05 21:38:05 +0800

    优化，添加新功能

--- js/main.js
+++ js/main.js
@@ -7,31 +7,67 @@ makeFakeSlides()
 $slides.css({ transform: 'translateX(-400px)' })
 bindEvents()
 
+// 上一张和下一张
+$(next).on('click', function () {
+  goToSlide(current + 1)
+})
+$(previous).on('click', function () {
+  goToSlide(current - 1)
+})
+
+// 自动轮播
+let timer = setInterval(function () {
+  goToSlide(current + 1)
+}, 2000)
+
+// 鼠标悬停暂停，移除鼠标继续
+$('.container').on('mouseenter', function () {
+  window.clearInterval(timer)
+}).on('mouseleave', function () {
+  timer = setInterval(function () {
+    goToSlide(current + 1)
+  }, 2000)
+})
+
+// 点击跳转
 function bindEvents () {
   $('#buttonWrapper').on('click', 'button', function (e) {
     let $button = $(e.currentTarget)
     let index = $button.index()
-    if (current === $buttons.length - 1 && index === 0) {
-      // 从最后一张到第一张
-      $slides.css({ transform: `translateX(${-($buttons.length + 1) * 400}px)` })
-      .one('transitionend', function () {
-        $slides.hide().offset()
-        $slides.css({ transform: `translateX(${-(index + 1) * 400}px)` }).show()
-      })
-    } else if (current === 0 && index === $buttons.length - 1) {
-      // 从第一张到最后一张
-      $slides.css({ transform: `translateX(0px)` })
-      .one('transitionend', function () {
-        $slides.hide().offset()
-        $slides.css({ transform: `translateX(${-(index + 1) * 400}px)` }).show()
-      })
-    } else {
-      $slides.css({ transform: `translateX(${-(index + 1) * 400}px)` })
-    }
-    current = index
+    goToSlide(index)
   })
 }
 
+// 切换图片
+function goToSlide (index) {
+  console.log(index)
+  if (index > $buttons.length - 1) {
+    index = 0
+  } else if (index < 0) {
+    index = $buttons.length - 1
+  }
+
+  if (current === $buttons.length - 1 && index === 0) {
+    // 从最后一张到第一张
+    $slides.css({ transform: `translateX(${-($buttons.length + 1) * 400}px)` })
+    .one('transitionend', function () {
+      $slides.hide().offset()
+      $slides.css({ transform: `translateX(${-(index + 1) * 400}px)` }).show()
+    })
+  } else if (current === 0 && index === $buttons.length - 1) {
+    // 从第一张到最后一张
+    $slides.css({ transform: `translateX(0px)` })
+    .one('transitionend', function () {
+      $slides.hide().offset()
+      $slides.css({ transform: `translateX(${-(index + 1) * 400}px)` }).show()
+    })
+  } else {
+    $slides.css({ transform: `translateX(${-(index + 1) * 400}px)` })
+  }
+  current = index
+}
+
+// 添加第一张图片和最后一张图片
 function makeFakeSlides () {
   let $firstCopy = $images.eq(0).clone(true)
   let $lastCopy = $images.eq($images.length - 1).clone(true)
